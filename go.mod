cca87cdcf6311e1d34caa79bbd944fcf85cfff3e
F-Bh <72809273+F-bh@users.noreply.github.com>
2022-12-29 22:33:15 +0100

    added fuzzy string matching

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module Duplicatefinder
+
+go 1.19
+
+require github.com/schollz/closestmatch v2.1.0+incompatible
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"github.com/schollz/closestmatch"
 	"io"
 	"log"
 	"os"
@@ -20,7 +21,7 @@ func main() {
 	hashMap := make(map[string]string)
 	dupeHashMap := make(map[string]string)
 
-	for _, entry := range entries {
+	for i, entry := range entries {
 		if entry.IsDir() {
 			continue
 		}
@@ -57,12 +58,27 @@ func main() {
 			continue
 		}
 
-		for k, v := range dupeHashMap {
-			fmt.Printf("found duplicate hash for: '%s' - '%s'\n", k, v)
+		var names []string
+		for j, ent := range entries {
+			if i == j {
+				continue
+			}
+			names = append(names, ent.Name())
 		}
 
-		for k, v := range dupeLenMap {
-			fmt.Printf("found duplicate length for: '%s' - '%s'\n", k, v)
+		cm := closestmatch.New(names, []int{2})
+		closest := cm.ClosestN(entry.Name(), 2)
+		for _, match := range closest {
+			fmt.Printf("found possible match: %s - %s", entry.Name(), match)
 		}
+
+	}
+
+	for k, v := range dupeHashMap {
+		fmt.Printf("found duplicate hash for: '%s' - '%s'\n", k, v)
+	}
+
+	for k, v := range dupeLenMap {
+		fmt.Printf("found duplicate length for: '%s' - '%s'\n", k, v)
 	}
 }
