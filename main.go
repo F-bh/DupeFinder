4a756fb8727ccd999b8aa624cdb89a2c20d296f7
F-Bh <72809273+F-bh@users.noreply.github.com>
2022-12-29 21:24:15 +0100

    hello world

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	entries, err := os.ReadDir("./")
+	if err != nil {
+		log.Fatal("could not read dir")
+	}
+
+	lenMap := make(map[int64]string)
+	dupeLenMap := make(map[string]string)
+	hashMap := make(map[string]string)
+	dupeHashMap := make(map[string]string)
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+
+		info, err := entry.Info()
+		if err != nil {
+			log.Fatal("could not read file info for: " + entry.Name())
+		}
+
+		name, ok := lenMap[info.Size()]
+		if !ok {
+			lenMap[info.Size()] = entry.Name()
+		} else {
+			dupeLenMap[entry.Name()] = name
+		}
+
+		file, err := os.Open("./" + entry.Name())
+		if err != nil {
+			panic("could not read file: " + entry.Name())
+		}
+
+		h := sha256.New()
+		if _, err := io.Copy(h, file); err != nil {
+			log.Fatal(err)
+		}
+
+		//hash
+		name, ok = hashMap[hex.EncodeToString(h.Sum(nil))]
+
+		if !ok {
+			hashMap[hex.EncodeToString(h.Sum(nil))] = entry.Name()
+		} else {
+			dupeHashMap[entry.Name()] = name
+			continue
+		}
+
+		for k, v := range dupeHashMap {
+			fmt.Printf("found duplicate hash for: '%s' - '%s'", k, v)
+		}
+
+		for k, v := range dupeLenMap {
+			fmt.Printf("found duplicate length for: '%s' - '%s'", k, v)
+		}
+	}
+}
